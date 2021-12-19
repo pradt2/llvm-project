cad41d9bda516caf8f7d6536e319c5c7fb0aa780
pradt2 <12902844+pradt2@users.noreply.github.com>
2021-12-19 17:07:07 +0000

    Automatic type substitution WIP

---
+++ clang/testcases/01_global_var_decl.cpp
@@ -0,0 +1,7 @@
+//TranslationUnitDecl 0x17c5208 <<invalid sloc>> <invalid sloc>
+//`-VarDecl 0x180b288 <./testcases/01_global_var_decl.cpp:4:1, col:5> col:5 packedDataObj 'int'
+
+long packedDataObj;
+long *packedDataObjPtr;
+long **packedDataObjPtr2;
+long &&packedDataObjRef2 = 0;
