c509eb44d87aadffbda7747dd2ffae1dfa6355c3
pradt2 <12902844+pradt2@users.noreply.github.com>
2021-12-29 00:39:24 +0000

    Source rewrite Impl 1 done

--- clang/testcases/01_global_var_decl.cpp
+++ clang/testcases/01_global_var_decl.cpp
@@ -1,7 +1,9 @@
 //TranslationUnitDecl 0x17c5208 <<invalid sloc>> <invalid sloc>
 //`-VarDecl 0x180b288 <./testcases/01_global_var_decl.cpp:4:1, col:5> col:5 packedDataObj 'int'
 
-long packedDataObj;
-long *packedDataObjPtr;
-long **packedDataObjPtr2;
-long &&packedDataObjRef2 = 0;
+#include "00_replaceable_types.h"
+
+ReplaceableSimpleStruct packedDataObj;
+ReplaceableSimpleStruct *packedDataObjPtr;
+ReplaceableSimpleStruct **packedDataObjPtr2;
+ReplaceableSimpleStruct &&packedDataObjRef2 = {};
