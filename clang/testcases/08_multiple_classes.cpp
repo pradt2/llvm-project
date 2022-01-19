d08b64e5e60ed8345242fc80b1d71485ff5934ce
pradt2 <12902844+pradt2@users.noreply.github.com>
2022-01-19 19:40:05 +0000

    Added support for non-compressed fields

--- clang/testcases/08_multiple_classes.cpp
+++ clang/testcases/08_multiple_classes.cpp
@@ -5,9 +5,13 @@ struct ParticleA {
   char particleASpeed;
   [[clang::compress_range(0, 16)]]
   char particleAAge;
+
+  int id;
 };
 
 struct ParticleB {
+  short id;
+
   [[clang::compress_range(0, 16)]]
   char particleBSpeed;
   [[clang::compress_range(0, 16)]]
@@ -15,16 +19,18 @@ struct ParticleB {
 };
 
 
-void printParticle(ParticleA &p) { printf("Particle A(size=%lu, speed=%d, age=%d)\n", sizeof p, p.particleASpeed, p.particleAAge); }
-void printParticle(ParticleB &p) { printf("Particle B(size=%lu, speed=%d, age=%d)\n", sizeof p, p.particleBSpeed, p.particleBAge); }
+void printParticle(ParticleA &p) { printf("Particle A(size=%lu, speed=%d, age=%d, id=%d)\n", sizeof p, p.particleASpeed, p.particleAAge, p.id); }
+void printParticle(ParticleB &p) { printf("Particle B(size=%lu, speed=%d, age=%d, id=%d)\n", sizeof p, p.particleBSpeed, p.particleBAge, p.id); }
 
 int main() {
-  ParticleA a { .particleASpeed = 10, .particleAAge = 11 };
+  ParticleA a { .particleASpeed = 10, .particleAAge = 11, .id = 111 };
   printParticle(a);
-  ParticleB b { .particleBSpeed = 14, .particleBAge = 15 };
+  ParticleB b { .particleBSpeed = 14, .particleBAge = 15, .id = 115 };
   printParticle(b);
   a.particleASpeed = b.particleBSpeed;
   b.particleBAge = a.particleAAge;
+  a.id = 77;
+  b.id = 77;
   printParticle(a);
   printParticle(b);
 }
