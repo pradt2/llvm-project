323fe30ad39ec6a28b7240c798f4df2ce7327879
pradt2 <12902844+pradt2@users.noreply.github.com>
2022-01-19 20:03:05 +0000

    #pragma pack compresses the fields just like the rewrite approach would

--- clang/testcases/08_bitfield.cpp
+++ clang/testcases/08_bitfield.cpp
@@ -5,12 +5,16 @@ struct StructA {
   bool b : 1;
 };
 
+enum X{a,b};
+#pragma pack(push, 1)
 struct StructB {
   unsigned short int0: 1 = 1, b : 2;
   unsigned char intA : 1;
   unsigned char intB : 1;
   bool boolA : 1;
+  X a : 2;
 };
+#pragma pack(pop)
 
 int main() {
   printf("Value of a: %d\n", StructA().a);
