d98291977a9b12459bedf9f95c5c04704ec2555a
pradt2 <12902844+pradt2@users.noreply.github.com>
2022-02-17 21:44:43 +0000

    Fixed arr access bug

--- clang/testcases/13_array_support.cpp
+++ clang/testcases/13_array_support.cpp
@@ -1,15 +1,22 @@
-#include <stdio.h>
-//void printf(const char* s, int i, int b);
+//#include <stdio.h>
+void printf(const char* s, int i, int b);
 
 struct S {
 
   [[clang::compress]]
   bool aTable[4][4];
 
+  [[clang::compress]]
+  bool x;
+
 };
 
 int main() {
   S s = {};
   s.aTable[1][2] = true;
+  S s2 = {};
+  s2.aTable[1][3] = s.aTable[1][2];
+  s2.x = s.x;
+  bool x = s.aTable[1][2];
   printf("Size %lu, val: %d\n", sizeof(s), s.aTable[1][2]);
 }
