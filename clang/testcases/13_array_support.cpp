65a1fbe21a77ad802239817223ccc571564901f8
pradt2 <12902844+pradt2@users.noreply.github.com>
2022-03-08 19:58:54 +0000

    Commit

--- clang/testcases/13_array_support.cpp
+++ clang/testcases/13_array_support.cpp
@@ -1,5 +1,5 @@
 //#include <stdio.h>
-void printf(const char* s, int i, int b);
+void printf(const char* s, int i, int b, int c);
 
 struct S {
 
@@ -18,5 +18,5 @@ int main() {
   s2.aTable[1][3] = s.aTable[1][2];
   s2.x = s.x;
   bool x = s.aTable[1][2];
-  printf("Size %lu, val: %d\n", sizeof(s), s.aTable[1][2]);
+  printf("Size %lu, val1: %d, val2: %d\n", sizeof(s), x, s.aTable[1][2]);
 }
