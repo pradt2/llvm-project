f86d88949144f9e3fb50e24c75a894fb8ef283ae
pradt2 <12902844+pradt2@users.noreply.github.com>
2022-07-24 19:06:08 +0100

    Safeguard against improper use of packing attrs

--- clang/testcases/18_paper_example.cpp
+++ clang/testcases/18_paper_example.cpp
@@ -1,32 +1,48 @@
 #include <stdio.h>
+#include <bitset>
 
-enum MoveState {
-  A, B, C
-};
+#define TwoPowerD 256
 
-struct Particle {
-  [[clang::pack]]
-  MoveState moveState;
-
-#ifdef ITERATION_LIMIT
-  [[clang::pack_range(0, ITERATION_LIMIT)]]
-#endif
-  int iterationCount;
-
-  [[clang::truncate_mantissa(7)]]
-  float convergenceResidual = 1.0f;
-
-  double energy;
+struct S {
+  [[clang::pack]] std::bitset<TwoPowerD> bitset;
+  [[clang::pack]] bool b;
 };
 
 int main() {
-  Particle p;
-  p.moveState = MoveState::B;
-  printf("moveState = %d\n", p.moveState);
-  p.iterationCount = 128;
-  printf("iterationCount = %d\n", p.iterationCount);
-  p.convergenceResidual = 1.125f;
-  printf("convergenceResidual = %f\n", p.convergenceResidual);
-  p.energy = 2e5f;
-  printf("energy %f\n", p.energy);
+  S s;
+  s.bitset.flip(0);
+  printf("Bool val: %d\n", s.bitset.test(0));
+  printf("Size s: %lu\n", sizeof s);
 }
+
+//enum MoveState {
+//  A, B, C
+//};
+//
+//struct Particle {
+//  [[clang::pack]]
+//  MoveState moveState;
+//
+//#ifdef ITERATION_LIMIT
+//  [[clang::pack_range(0, ITERATION_LIMIT)]]
+//#endif
+//  int iterationCount;
+//
+//  [[clang::truncate_mantissa(7)]]
+//  float convergenceResidual = 1.0f;
+//
+//  double energy;
+//};
+//
+//int main() {
+//  Particle p;
+//  p.moveState = MoveState::B;
+//  printf("moveState = %d\n", p.moveState);
+//  p.iterationCount = 128;
+//  printf("iterationCount = %d\n", p.iterationCount);
+//  p.convergenceResidual = 1.125f;
+//  printf("convergenceResidual = %f\n", p.convergenceResidual);
+//  p.energy = 2e5f;
+//  printf("energy %f\n", p.energy);
+//}
+
