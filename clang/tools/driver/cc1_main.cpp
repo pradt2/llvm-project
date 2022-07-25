d539ec1c4a6b9870bf4e4e1e5a8058ba560f2fd8
pradt2 <12902844+pradt2@users.noreply.github.com>
2022-07-25 21:10:52 +0100

    Compiler flags and macros

--- clang/tools/driver/cc1_main.cpp
+++ clang/tools/driver/cc1_main.cpp
@@ -276,7 +276,8 @@ int cc1_main(ArrayRef<const char *> Argv, const char *Argv0, void *MainAddr) {
     }
 
     if (Result == FRONTEND_ACTION_SUCCESS && Clang->getLangOpts().PackedAttributesLanguageExtension) {
-      rewrittenSourcesHandler.loadRewrittenSources(CompilerInstance->getSourceManager().getRewriter());
+      if (CompilerInstance.get())
+        rewrittenSourcesHandler.loadRewrittenSources(CompilerInstance->getSourceManager().getRewriter());
 
       CompilerInstance = CreateCompilerInstance(Argv, Argv0, MainAddr);
       rewrittenSourcesHandler.saveIntoOpts(CompilerInstance->getPreprocessorOpts());
@@ -289,7 +290,8 @@ int cc1_main(ArrayRef<const char *> Argv, const char *Argv0, void *MainAddr) {
     }
 
     if (Result == FRONTEND_ACTION_SUCCESS && Clang->getLangOpts().MpiAttributesLanguageExtension) {
-      rewrittenSourcesHandler.loadRewrittenSources(CompilerInstance->getSourceManager().getRewriter());
+      if (CompilerInstance.get())
+        rewrittenSourcesHandler.loadRewrittenSources(CompilerInstance->getSourceManager().getRewriter());
 
       CompilerInstance = CreateCompilerInstance(Argv, Argv0, MainAddr);
       rewrittenSourcesHandler.saveIntoOpts(CompilerInstance->getPreprocessorOpts());
@@ -302,7 +304,8 @@ int cc1_main(ArrayRef<const char *> Argv, const char *Argv0, void *MainAddr) {
     }
 
     if (Result == FRONTEND_ACTION_SUCCESS) {
-      rewrittenSourcesHandler.loadRewrittenSources(CompilerInstance->getSourceManager().getRewriter());
+      if (CompilerInstance.get())
+        rewrittenSourcesHandler.loadRewrittenSources(CompilerInstance->getSourceManager().getRewriter());
       rewrittenSourcesHandler.saveIntoOpts(Clang->getPreprocessorOpts());
 
       rewrittenSourcesHandler.dump();
