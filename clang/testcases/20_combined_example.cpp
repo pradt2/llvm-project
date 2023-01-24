d24328f7f5643740adde17cb8ff18789d07ff9ba
pradt2 <12902844+pradt2@users.noreply.github.com>
2023-01-24 00:27:26 +0000

    Testcase

--- clang/testcases/20_combined_example.cpp
+++ clang/testcases/20_combined_example.cpp
@@ -1,15 +1,37 @@
-#include <stdio.h>
-#include <algorithm>
+//#include <functional>
+#include <unordered_set>
 
-double convertAbsoluteIntoRelativeValue(
-    double referenceValue,
-    double value
+namespace swift2 { namespace kernels {
+template <typename ParticleXX, typename ParticleContainer>
+void genericInteraction(
+    ParticleContainer &localParticles,
+    ParticleContainer &activeParticles);
+} }
+
+class Particle {
+  [[clang::pack]] bool a;
+};
+
+template <typename ParticleXX, typename ParticleContainer>
+void swift2::kernels::genericInteraction(
+    ParticleContainer&  localParticles,
+    ParticleContainer&  activeParticles
 ) {
-  const double weight = std::max(
-      1.0, std::abs(referenceValue)
-  );
-  return value / weight;
+  for (auto localParticle: localParticles ) {
+      for (auto activeParticle: activeParticles ) {
+
+      }
+  }
 }
 
+
+
 int main() {
+  std::unordered_set<Particle*> localParticles;
+  std::unordered_set<Particle*> activeParticles;
+
+  swift2::kernels::genericInteraction<Particle>(
+      localParticles, activeParticles
+      );
+
 }
