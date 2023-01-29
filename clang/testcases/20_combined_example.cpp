d1fa06bb5fb52ec58b2e8b5ee8035104b0f4cce0
pradt2 <12902844+pradt2@users.noreply.github.com>
2023-01-29 21:12:56 +0000

    Disable soa input-output type checking

--- clang/testcases/20_combined_example.cpp
+++ clang/testcases/20_combined_example.cpp
@@ -1,5 +1,3 @@
-#include <vector>
-
 struct Struct {
   bool a;
   bool b;
@@ -17,23 +15,35 @@ struct Struct {
   }
 };
 
+//using DATACLASS = Struct;
+
+#define TEMPLATED_MAIN(DATACLASS) { \
+                                    DATACLASS *dataOuter = new DATACLASS[10]; \
+                                    DATACLASS *dataInner = new DATACLASS[10]; \
+ \
+                                    [[clang::soa_conversion_target(dataOuter)]] \
+                                    [[clang::soa_conversion_target_size(10)]] \
+                                    [[clang::soa_conversion_data_item("getA()", "")]] \
+                                    [[clang::soa_conversion_data_item("getB()", "setB()")]] \
+                                    for (int i = 0; i < 10; i++) { \
+ \
+                                    [[clang::soa_conversion_target(dataInner)]] \
+                                    [[clang::soa_conversion_target_size(10)]] \
+                                    [[clang::soa_conversion_data_item("getB()", "")]] \
+                                    [[clang::soa_conversion_data_movement_strategy(move_to_outermost)]] \
+                                    for (int j = 0; j < 10; j++) { \
+                                    dataOuter[i].setB(dataOuter[i].getA() | dataInner[j].getB()); \
+                                    } \
+ \
+                                    } \
+}
+void templatedMain() {
+
+
+}
+
 int main() {
 
-  auto vectorOuter = std::vector<Struct*>();
-  auto vectorInner = std::vector<Struct*>();
-
-  [[clang::soa_conversion_target_size(vectorOuter.size())]]
-  [[clang::soa_conversion_data_item("getA()", "")]]
-  [[clang::soa_conversion_data_item("getB()", "setB()")]]
-  for (auto itemOuter : vectorOuter) {
-
-    [[clang::soa_conversion_target_size(vectorInner.size())]]
-    [[clang::soa_conversion_data_item("getB()", "")]]
-    [[clang::soa_conversion_data_movement_strategy(move_to_outermost)]]
-    for (auto itemInner: vectorInner) {
-      itemOuter->setB(itemOuter->getA() | itemInner->getB());
-    }
-
-  }
+  TEMPLATED_MAIN(Struct)
 
 }
