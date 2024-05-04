10957ac7f4d65ffade483cd4af21633b4fd12b16
pradt2 <12902844+pradt2@users.noreply.github.com>
2024-05-04 15:48:17 +0100

    soa conversions: restore support for CXXForRangeStmt

--- clang/testcases/17_soa.cpp
+++ clang/testcases/17_soa.cpp
@@ -1,3 +1,7 @@
+#include "alloca.h"
+
+#include <vector>
+
 struct Particle {
     struct {
         double x[3];
@@ -7,16 +11,31 @@ struct Particle {
 
 extern double sqrt(double);
 
-void doWork(Particle *particles, int size) {
+//void doWork(Particle *particles, int size) {
+//
+//    [[clang::soa_conversion_target(particles)]]
+//    [[clang::soa_conversion_target_size(size)]]
+//    [[clang::soa_conversion_data_item("force.x[0]", "force.x[0]")]]
+//    [[clang::soa_conversion_data_item("force.x[1]", "force.x[1]")]]
+//    [[clang::soa_conversion_data_item("force.x[2]", "force.x[2]")]]
+//    [[clang::soa_conversion_data_item("d", "d")]]
+//    [[clang::soa_conversion_allocation_strategy(stack)]]    // remember about #include "alloca.h"
+//    for (int i = 0; i < size; i++) {
+//        auto dist = sqrt(particles[i].force.x[0] * particles[i].force.x[1] * particles[i].force.x[2]);
+//        particles[i].d = dist;
+//    }
+//}
 
-    [[clang::soa_conversion_target(particles)]]
-    [[clang::soa_conversion_target_size(size)]]
+void doMoreWork(std::vector<Particle> &particles) {
+
+    [[clang::soa_conversion_target_size(particles.size())]]
     [[clang::soa_conversion_data_item("force.x[0]", "force.x[0]")]]
     [[clang::soa_conversion_data_item("force.x[1]", "force.x[1]")]]
     [[clang::soa_conversion_data_item("force.x[2]", "force.x[2]")]]
     [[clang::soa_conversion_data_item("d", "d")]]
-    for (int i = 0; i < size; i++) {
-        auto dist = sqrt(particles[i].force.x[0] * particles[i].force.x[1] * particles[i].force.x[2]);
-        particles[i].d = dist;
+    [[clang::soa_conversion_allocation_strategy(heap)]]    // remember about #include "alloca.h"
+    for (auto &p : particles) {
+        auto dist = sqrt(p.force.x[0] * p.force.x[1] * p.force.x[2]);
+        p.d = dist;
     }
 }
