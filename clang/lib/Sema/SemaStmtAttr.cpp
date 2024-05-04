132e2d9f6cd58c3d9f5b3b415aeee2ec3457b91b
pradt2 <12902844+pradt2@users.noreply.github.com>
2024-05-04 15:08:30 +0100

    add soa_conversion_allocation_strategy attr

--- clang/lib/Sema/SemaStmtAttr.cpp
+++ clang/lib/Sema/SemaStmtAttr.cpp
@@ -408,6 +408,34 @@ static Attr *handleSoaConversionDataMovementStrategy(Sema &S, Stmt *St, const Pa
   exit(1);
 }
 
+static Attr *handleSoaConversionAllocationStrategy(Sema &S, Stmt *St, const ParsedAttr &A,
+                                                   SourceRange Range) {
+    auto argsSize = A.getNumArgs();
+
+    if (argsSize != 1) {
+        llvm::errs() << __FILE__ << ":" << __LINE__ << "Attribute should have 1 arg";
+        exit(1);
+    }
+
+    auto *arg = A.getArgAsIdent(0);
+    auto strategyName = arg->Ident->getName();
+
+    if (strategyName == "stack") {
+        auto type = SoaConversionAllocationStrategyAttr::AllocationStrategyType::Stack;
+        return ::new (S.Context) SoaConversionAllocationStrategyAttr(S.Context, A, type);
+    }
+    if (strategyName == "heap") {
+        auto type = SoaConversionAllocationStrategyAttr::AllocationStrategyType::Heap;
+        return ::new (S.Context) SoaConversionAllocationStrategyAttr(S.Context, A, type);
+    }
+    if (strategyName == "vla") {
+        auto type = SoaConversionAllocationStrategyAttr::AllocationStrategyType::VLA;
+        return ::new (S.Context) SoaConversionAllocationStrategyAttr(S.Context, A, type);
+    }
+    llvm::errs() << __FILE__ << ":" << __LINE__ << "Unknown data movement strategy";
+    exit(1);
+}
+
 CodeAlignAttr *Sema::BuildCodeAlignAttr(const AttributeCommonInfo &CI,
                                         Expr *E) {
   if (!E->isValueDependent()) {
@@ -701,6 +729,8 @@ static Attr *ProcessStmtAttribute(Sema &S, Stmt *St, const ParsedAttr &A,
     return handleSoaConversionTargetSize(S, St, A, Range);
   case ParsedAttr::AT_SoaConversionDataMovementStrategy:
     return handleSoaConversionDataMovementStrategy(S, St, A, Range);
+  case ParsedAttr::AT_SoaConversionAllocationStrategy:
+    return handleSoaConversionAllocationStrategy(S, St, A, Range);
   case ParsedAttr::AT_CodeAlign:
     return handleCodeAlignAttr(S, St, A);
   case ParsedAttr::AT_MSConstexpr:
